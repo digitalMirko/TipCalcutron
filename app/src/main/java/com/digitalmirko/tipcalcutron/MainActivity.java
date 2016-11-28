ffeb25a545ae6c782ff880449b664b5cbdd7eeb6
Mirko Cukich <mirkocukich@gmail.com>
2016-11-28 15:50:13 -0600

    Connected textView Item & Tested

--- app/src/main/java/com/digitalmirko/tipcalcutron/MainActivity.java
+++ app/src/main/java/com/digitalmirko/tipcalcutron/MainActivity.java
@@ -2,12 +2,19 @@ package com.digitalmirko.tipcalcutron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tipTotalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tipTotalTextView = (TextView) findViewById(R.id.textView3);
+
+        tipTotalTextView.setText("Testing123");
     }
 }
