a9313eb0cc0098e8c5d76401fe9f9370b0e169fd
Mirko Cukich <mirkocukich@gmail.com>
2016-11-28 14:50:03 -0600

    Initial commit

---
+++ app/src/main/java/com/digitalmirko/tipcalcutron/MainActivity.java
@@ -0,0 +1,13 @@
+package com.digitalmirko.tipcalcutron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
