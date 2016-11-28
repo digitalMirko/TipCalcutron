31003beb631d74c5633c91435032e94156346c5b
Mirko Cukich <mirkocukich@gmail.com>
2016-11-28 16:10:43 -0600

    Created Variables & Connected Assorted Items

--- app/src/main/java/com/digitalmirko/tipcalcutron/MainActivity.java
+++ app/src/main/java/com/digitalmirko/tipcalcutron/MainActivity.java
@@ -2,19 +2,46 @@ package com.digitalmirko.tipcalcutron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Bill Total EditText
+    EditText billTotalEditText;
+
+    //  5, 10, 15 & 20 Percentage Tip Buttons
+    Button fivePercentBtn;
+    Button tenPercentBtn;
+    Button fifteenPercentBtn;
+    Button twentyPercentBtn;
+
+    // Tip & Total TextView
     TextView tipTotalTextView;
+    TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tipTotalTextView = (TextView) findViewById(R.id.textView3);
+        // Bill Total EditText connection
+        billTotalEditText = (EditText) findViewById(R.id.billTotalEditText);
 
-        tipTotalTextView.setText("Testing123");
+        // 5, 10, 15 & 20 Percentage Tip Buttons connection
+        fivePercentBtn = (Button) findViewById(R.id.fiveBtn);
+        tenPercentBtn = (Button) findViewById(R.id.tenBtn);
+        fifteenPercentBtn = (Button) findViewById(R.id.fifteenBtn);
+        twentyPercentBtn = (Button) findViewById(R.id.twentyBtn);
+
+        // Tip & Total TextView connections
+        tipTotalTextView = (TextView) findViewById(R.id.tipTotalTextView);
+//        tipTotalTextView.setText("Testing123"); // tested connection
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+
+
+
     }
 }
